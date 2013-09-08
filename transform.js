11137c1228578e94b6c3eb62763b0730607d0063
Arne Martin Aurlien <am@aurlien.net>
2013-09-08 04:13:59 +0200

    Implement getting and setting element transform

--- transform.js
+++ transform.js
@@ -1,14 +1,46 @@
+var part = function(string, which) {
+    var regex = new RegExp(which + '\\((-?\\d+(\\.\\d+)?\\w*)\\)');
+    var result = string.match(regex);
+    if (result && result[1]) {
+        return parseFloat(result[1]);
+    } else {
+        return 0;
+    }
+};
+
 module.exports = {
-    set: function(element, x, y, zoom, rotation) {
-    
+    set: function(element, pos) {
+        var transform = 'translateX(' + pos.x + 'px) ' +
+                        'translateY(' + pos.y + 'px) ' +
+                        'scale(' + pos.scale + ') ' +
+                        'rotate(' + pos.rotate + 'deg)';
+        element.style.WebkitTransform = transform;
+        element.style.MozTransform = transform;
+        element.style.OTransform = transform;
+        element.style.transform = transform;
         return element;
     },
+
     get: function(element) {
-        return {
-            x: 0,
-            y: 0,
-            zoom: 1,
-            rotation: 0
-        };
+        var transform = element.style.WebkitTransform ||
+                        element.style.MozTransform ||
+                        element.style.OTransform ||
+                        element.style.transform;
+        
+        if (transform && transform.length) {
+            return {
+                x: part(transform, 'translateX'),
+                y: part(transform, 'translateY'),
+                scale: part(transform, 'scale'),
+                rotate: part(transform, 'rotate')
+            };
+        } else {
+            return {
+                x: 0,
+                y: 0,
+                scale: 1,
+                rotate: 0
+            }
+        }
     }
-}
+};
