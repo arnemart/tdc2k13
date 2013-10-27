c91fd0f06267bbafaa3befa6561d0fc635b4f249
Arne Martin Aurlien <am+github@aurlien.net>
2013-10-27 12:37:57 +0100

    Remove som code from demo version of pos.js

--- _js/pos.js
+++ _js/pos.js
@@ -1,35 +1,7 @@
 module.exports = {
     // Get position(s) from an event
     pos: function(evt) {
-        // Double touch
-        if (evt.targetTouches && evt.targetTouches.length === 2) {
-            var x = evt.targetTouches[0].clientX;
-            var y = evt.targetTouches[0].clientY;
-            var deltaX = evt.targetTouches[1].clientX - x;
-            var deltaY = evt.targetTouches[1].clientY - y;
-            return {
-                multi: true,
-                x: x,
-                y: y,
-                // Math!
-                distance: Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2)),
-                rotate: Math.atan2(deltaY, deltaX) * 180 / Math.PI
-            };
-
-        // Single touch
-        } else if (evt.targetTouches && (evt.targetTouches.length === 1 || evt.targetTouches.length > 2)) {
-            return {
-                x: evt.targetTouches[0].clientX,
-                y: evt.targetTouches[0].clientY
-            };
-
-        // No touch events
-        } else if (!evt.targetTouches) {
-            return {
-                x: evt.clientX,
-                y: evt.clientY
-            };
-        }
+        // DO SOME THINGS
     },
 
     // Calculcate delta between two positions
